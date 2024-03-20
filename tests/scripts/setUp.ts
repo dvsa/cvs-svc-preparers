95970489e3e34552cf415df69a03aef5a383d773
owen <owen.corrigan@bjss.com>
2024-03-20 10:44:50 +0000

    feat(cb2-11223): set up script changes

--- tests/scripts/setUp.ts
+++ tests/scripts/setUp.ts
@@ -1,7 +1,7 @@
-import { exec } from "child_process";
+import { exec } from 'child_process';
 
 // We hook to serverless offline when firing its process
-const SERVER_OK = `Server ready: http://localhost:3003 🚀`;;
+const SERVER_OK = `Server ready: http://localhost:3003 🚀`;
 // Serverless fires a local dynamo-db instance which is killed once the parent process is terminated
 // the current serverless script checks whether a local instance is running but does not error when binding fails
 // we force throwing an error so we always start from a clean slate if java.io.IOException: Failed to bind to 0.0.0.0/0.0.0.0:8006
@@ -9,24 +9,23 @@ const DYNAMO_LOCAL_ERROR_THREAD = `Exception in thread "main"`;
 
 const setupServer = (process: any) => {
   return new Promise((resolve, reject) => {
-    process.stdout.setEncoding("utf-8").on("data", (stream: any) => {
+    process.stdout.setEncoding('utf-8').on('data', (stream: any) => {
       console.log(`stdout: ${stream}`);
       if (stream.includes(SERVER_OK)) {
         resolve(process);
       }
     });
 
-    process.stderr.setEncoding("utf-8").on("data", (stream: any) => {
+    process.stderr.setEncoding('utf-8').on('data', (stream: any) => {
       console.log(`stderr: ${stream}`);
       if (stream.includes(DYNAMO_LOCAL_ERROR_THREAD)) {
-        throw new Error("Internal Java process crashed");
-      }
-      else if (stream.includes(SERVER_OK)) {
+        throw new Error('Internal Java process crashed');
+      } else if (stream.includes(SERVER_OK)) {
         resolve(process);
       }
     });
 
-    process.on("exit", (code: any, signal: any) => {
+    process.on('exit', (code: any, signal: any) => {
       if (code !== 137) {
         console.info(`process terminated with code: ${code} and signal: ${signal}`);
       }
@@ -34,7 +33,7 @@ const setupServer = (process: any) => {
   });
 };
 
-const server = exec("npm run start &", (error) => {
+const server = exec('npm run start &', (error) => {
   if (error) {
     console.error(`error starting server: ${error}`);
   }
@@ -51,7 +50,7 @@ module.exports = async () => {
     on pid: ${pid}
     `);
   } catch (e) {
-    console.error("Something wrong happened:\n");
+    console.error('Something wrong happened:\n');
     console.error(e);
     process.exit(1);
   }
